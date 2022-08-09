7b0165c7f932b7732747423ca2d28f1c39e63a1b
GiraffeM9 <76228570+GiraffeM9@users.noreply.github.com>
2022-08-09 20:21:59 +1200

    modified:   PetChat/website/__pycache__/views.cpython-310.pyc 	modified:   PetChat/website/static/stylesheet.css 	modified:   PetChat/website/templates/404.html 	modified:   PetChat/website/templates/index.html 	modified:   PetChat/website/views.py

--- PetChat/website/views.py
+++ PetChat/website/views.py
@@ -19,7 +19,8 @@ def home():
 def newsletter():
     if request.method == 'POST':
         pass
-    return render_template("newsletter.html", name=current_user.username, mail_count=9, newsletters=5)
+    news_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ullamcorper bibendum erat in condimentum. Fusce mauris ex, faucibus sit amet lobortis eu, dapibus at urna. Nam vel urna lectus. Suspendisse mattis erat quam, a ullamcorper erat lobortis quis. In eros libero, imperdiet non rhoncus placerat, porta et dolor. Fusce sit amet velit eleifend, tristique lacus a, accumsan diam. Praesent ac lacus vel turpis imperdiet scelerisque. Nullam ac enim non nulla sodales varius. Suspendisse finibus porta elit, a bibendum est tristique eget. Mauris ac augue at velit tincidunt efficitur rhoncus sit amet lectus."
+    return render_template("newsletter.html", name=current_user.username, mail_count=9, newsletters=5, news_content=news_content)
 
 
 @views.route("/events", methods=["GET", "POST"])
